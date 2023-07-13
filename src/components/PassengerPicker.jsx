5524fed6d701fe9eef1974519aab16787aa72b48
Tole15 <rodrigermoon@gmail.com>
2023-07-13 16:00:03 -0600

    Refactor the PassengerPicker component to improve the handling of item selection and value handling. - Added a new function, `handleItemSelected`, to handle the selection of an item from the wheel picker. - Updated the `onItemSelected` prop of the WheelPicker component to use the `handleItemSelected` function. - Created a new variable, `selectedIndex`, to store the index of the selected value in the `numbPassengers` array. - Created a new variable, `selectedItem`, to determine the default selected item if the value is not found in the `numbPassengers` array. - Updated the `selectedItem` prop of the WheelPicker component to use the `selectedItem` variable.
    These changes ensure that the selected item is properly handled and that the component functions as expected.
    
    Co-authored-by: BrandonVG <brandon.vadillox3@gmail.com>
    Co-authored-by: Axw3ll <axelomar.a.v@gmail.com>

--- src/components/PassengerPicker.jsx
+++ src/components/PassengerPicker.jsx
@@ -1,17 +1,23 @@
-import React, {useState} from 'react';
-import {View, Text, S} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
 import {WheelPicker} from 'react-native-wheel-picker-android';
-
 
 const PassengerPicker = ({setSelectItem, value}) => {
   const numbPassengers = ['1', '2', '3', '4', '5'];
 
+  const handleItemSelected = index => {
+    setSelectItem(Number(numbPassengers[index]));
+  };
+
+  const selectedIndex = numbPassengers.indexOf(String(value));
+  const selectedItem = selectedIndex === -1 ? 0 : selectedIndex;
+
   return (
     <View style={{alignSelf: 'center', marginTop: 30}}>
       <WheelPicker
-        selectedItem={value}
+        selectedItem={selectedItem}
         data={numbPassengers}
-        onItemSelected={setSelectItem}
+        onItemSelected={handleItemSelected}
         indicatorColor="#5974f5"
         isCyclic={true}
         indicatorWidth={3}
